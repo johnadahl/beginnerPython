18a7b3c40c8717cb142d3302efd0d9e3c43530f2
johndahl <johnadahl@protonmail.com>
2021-02-24 19:47:53 -0700

    I think I'm getting the hang of this, but I'm still so slow. I'm getting closer to thinking like a programmer though.

--- Biphasicsleepcalculator.py
+++ Biphasicsleepcalculator.py
@@ -1,3 +1,8 @@
 #This is a biphasic sleep schedule creator
-
+#We will have sleep be the 6 hr sleep, and nap be the 30 min sleep.
 print("Biphasic sleep is sleeping twice per day instead of once. \nIt allows for less total sleep but the same feeling of being rested.")
+#What we want to do is allow someone to say when they want to go to bed, and have the program say the waking time, and the nap time.
+print("What hour do you want to sleep?")
+bois = int(input())
+print("What minute do you want to sleep within that hour? (10:30)")
+dumbs = int(input())
