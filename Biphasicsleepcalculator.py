9b4ae56f9c58378807f1d716455236257dacf0e0
johndahl <johnadahl@protonmail.com>
2021-02-24 19:31:27 -0700

    This is the start

---
+++ Biphasicsleepcalculator.py
@@ -0,0 +1,3 @@
+#This is a biphasic sleep schedule creator
+
+print("Biphasic sleep is sleeping twice per day instead of once. \nIt allows for less total sleep but the same feeling of being rested.")
