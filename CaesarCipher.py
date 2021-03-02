2768614b51e3d0dcf1ecc619b353d452c3db3169
johndahl <johnadahl@protonmail.com>
2021-03-02 10:05:42 -0700

    I sure don't undertsand what is going on in this program YET.

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -2,12 +2,21 @@
 #I found this at https://stackoverflow.com/questions/8886947/caesar-cipher-function-in-python
 
 #I need to get a string and shift it. I'm going for a brute force method here.
-def caesar(text, shift):
+print('Please Input your phrase to be translated.')
+translatethis = str(input())
+print('Please input your shift amount.')
+boyo = int(input())
+def caesar(plaintext, shift):
     #We need to take the text as an argument,
     #and the letter shift as anothe argument.
-    alphabet = string.ascii_lowercase
+    alphabet = str.lower
     #make it all lowercase.
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     #the [shift:] means shift and everything after the shift,
-    #and [:shift is everything before the shift.]
+    #and [:shift is everything before the shift, but excluding the shift]
     #I'm going to make another project that uses this later.
+    table = str.maketrans(alphabet, shifted_alphabet)
+
+    return plaintext.translate(table)
+    print(plaintext.translate(table))
+caesar(translatethis, boyo)
