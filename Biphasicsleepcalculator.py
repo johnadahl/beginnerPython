59bf10a2a2a7676605a26dedacfebf88c558ff93
johndahl <johnadahl@protonmail.com>
2021-03-17 10:01:55 -0600

    Everything in the biphasic sleep is breaking. I think I'm done with the caeser cipher for now, I'll fix it later. I'm trying to make the program myself, or at least the logic before I look it up on stack overfow. It's been a good exercise, but I'm very confused. I'll figure it out though.

--- Biphasicsleepcalculator.py
+++ Biphasicsleepcalculator.py
@@ -1,8 +1,16 @@
-#This is a biphasic sleep schedule creator
 #We will have sleep be the 6 hr sleep, and nap be the 30 min sleep.
 print("Biphasic sleep is sleeping twice per day instead of once. \nIt allows for less total sleep but the same feeling of being rested.")
 #What we want to do is allow someone to say when they want to go to bed, and have the program say the waking time, and the nap time.
-print("What hour do you want to sleep?")
-bois = int(input())
-print("What minute do you want to sleep within that hour? (10:30)")
-dumbs = int(input())
+print("What time do you want to sleep?")
+timeinbed = input()
+hourminute = timeinbed.split(':')
+
+
+print(hourminute)
+def sleepcalc(bedtime):
+    for a in bedtime:
+        bedtime[a] = int(bedtime[a]) + 6
+#    bedtime[0] += 6
+    print(bedtime)
+
+sleepcalc(hourminute)
