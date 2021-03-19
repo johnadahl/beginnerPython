49c42c3ee4325e0109c66caf9cbfa9cd7c4c653b
johndahl <johnadahl@protonmail.com>
2021-03-19 09:38:23 -0600

    I believe it works now! I even added some functionality to the program so it didn't fail using numbers like 12:12. I'm tired though, and I want to sleep. I'm happy this is working, and I'm ready to do something else. It's good to be alive in this wonderful world. I know God loves me. God is good.

--- Biphasicsleepcalculator.py
+++ Biphasicsleepcalculator.py
@@ -2,7 +2,10 @@
 print("Biphasic sleep is sleeping twice per day instead of once. \nIt allows for less total sleep but the same feeling of being rested.")
 #What we want to do is allow someone to say when they want to go to bed, and have the program say the waking time, and the nap time.
 print("What time do you want to sleep?")
-timeinbed = input()
+timeinbed = '12:12'
+#I'll return this to an input later. I can keep it at a chosen number
+#That would break the program so I can ensure it works without typing
+#An input all day.
 hourminute = timeinbed.split(':')
 hourminute = list(map(int, hourminute))
 #This somehow turns all the list items in list hourminute into integers.
@@ -12,8 +15,16 @@ bedtime = []
 print(hourminute)
 def sleepcalc(hourminute):
     #This is where it dies.
-    for a in hourminute:
-        hourminute[a] += 6
+    hourminute[0] += 6
+    if hourminute[1] < 15:
+        hourminute[0] -= 1
+        hourminute[1] = 60 + (hourminute[1] - 15)
+    else:
+        hourminute[1] -= 15
+    if hourminute[0] > 12:
+        hourminute[0] -= 12
+    else:
+        return
 #I still think the comments underneath should work.
 #I don't know why they don't.
 #    for a in hourminute:
