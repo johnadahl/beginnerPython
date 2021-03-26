6dba758211952de569348d11e81f3c3d6d98a6a2
John Dahl <johnadahl@protonmail.com>
2021-03-25 21:24:53 -0600

    The readme should be a .md instead of a .txt.
    I think that should give me more options later on.

--- Biphasicsleepcalculator.py
+++ Biphasicsleepcalculator.py
@@ -1,4 +1,4 @@
-#We will have sleep be the 6 hr sleep, and nap be the 30 min sleep.
+ #We will have sleep be the 6 hr sleep, and nap be the 30 min sleep.
 print("Biphasic sleep is sleeping twice per day instead of once. \nIt allows for less total sleep but the same feeling of being rested.")
 #What we want to do is allow someone to say when they want to go to bed, and have the program say the waking time, and the nap time.
 print("What time do you want to sleep?")
