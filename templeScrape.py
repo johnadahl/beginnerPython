7f7fc16f9ac958a24609004090ada9ca884cb526
John Dahl <johnadahl@protonmail.com>
2021-07-11 16:12:15 -0600

    I have what I want the temple scrape program to do written in comments in the top.

---
+++ templeScrape.py
@@ -0,0 +1,12 @@
+#Here I will write my code in pseudocode/make the idea.
+#Go to https://www.churchofjesuschrist.org/temples/schedule/appointment?lang=eng.
+#Click on Select This Temple (Button= Class Select this Temple)
+#Select Proxy Baptism
+#Male Patrons to be Baptised = 1
+#See Map
+#See if there are any avalible Dates
+#If there are, give me a notification involving the date/time/how many are needed to come to the temple.
+#Else, give me a notification saying there is are no openings with the option to mute until there is an opening.
+
+
+
