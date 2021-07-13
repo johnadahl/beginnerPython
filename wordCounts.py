a15a0d003c367c28c9c5d5096b5034d6a2a464c1
John Dahl <johnadahl@protonmail.com>
2021-07-12 20:59:30 -0600

    It still doesn't work.

--- wordCounts.py
+++ wordCounts.py
@@ -1,20 +1,60 @@
-count = dict()
-words = input('Paste your copy pasta. \n')
+counts = {}
+copyPasta = input('Please input your copy pasta. \n')
+wordsOfCopyPasta = copyPasta.split()
 
-wordsaslist = words.split()
-
-for word in wordsaslist:
-    if word in count:
-        count[word] += 1
+for word in wordsOfCopyPasta:
+    if word in counts:
+        counts[word] += 1
     else:
-        count[word] = 1
+        counts[word] = 1
+
+tmp = {}
+
+for (k,v) in counts:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#count = dict()
+#words = input('Paste your copy pasta. \n')
+
+#wordsaslist = words.split()
+
+#for word in wordsaslist:
+#    if word in count:
+#        count[word] += 1
+#    else:
+#        count[word] = 1
+
+
 # print(count)
 #for word in count:
 #    if count[word] > 10:
 #        print(count)
 #    else:
 #        continue
-chunk = {}
-for (word, number) in count:
-    chunk.append(number: word)
-print(chunk.sort(reversed = True))
+
+
+#chunk = {}
+#for (word, number) in count:
+#    chunk.append(number, word)
+#print(chunk.sort(reverse = True))
