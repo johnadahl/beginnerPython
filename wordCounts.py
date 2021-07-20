cf4d698a81daa8b4230c772ef0cdc0c7c50b666a
John Dahl <johnadahl@protonmail.com>
2021-07-19 19:38:37 -0600

    The tmp = sorted(tmp, reversed=True) thing turned tmp into a list that only kept the keys. This is a problem because I need bothe parts of the key value pairs in order to give both parts of the key value pair back to the user in a print statement. I will find the solution next time I program here, but for now, I need to go on to other things.

--- wordCounts.py
+++ wordCounts.py
@@ -1,5 +1,5 @@
 counts = {}
-copyPasta = input('Please input your copy pasta. \n')
+copyPasta = "The FitnessGram Pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter Pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal *boop*. A single lap should be completed each time you hear this sound *ding*. Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
 wordsOfCopyPasta = copyPasta.split()
 
 for word in wordsOfCopyPasta:
@@ -19,14 +19,21 @@ for (k,v) in counts.items():
 
 print(tmp)
 #This shows that the program works perfectly to the line above.
-tmp = sorted(tmp, reverse = True)
+#tmp = sorted(tmp, reverse = True)
+#So this turns tmp into a list instead of a dictionary like I wanted it to be. That is why it only printed out some of the data, that data being the key.
+tmp = tmp.sort()
+
 
 print(tmp)
 #I don't know why this doesn't print both the value and the key. It is only printing the key which is an integer. Super annoying.
 #Now I just need to make it get sorted and then pring only the first x number of words.
 
-for i in tmp():
-    print(i, tmp[i])
+for (k, v) in tmp.items():
+    print(v, k)
+
+
+#for i in tmp:
+#    print(i, tmp[i])
 
 
 
