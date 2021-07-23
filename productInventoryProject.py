ed0bcdf2802fe5241a4cb41fe4e8748143bd81b4
John Dahl <johnadahl@protonmail.com>
2021-07-22 19:59:38 -0600

    So I've broken some things, but I have to go to the stake center now.

--- productInventoryProject.py
+++ productInventoryProject.py
@@ -4,25 +4,32 @@
 #Then create an inventory class which keeps track of various products and can sum up the inventory value.
 #We're going to do a gpu shop. It should be easy because there is never any stock. Ha Ha.
 
-#So we have watched the firts video from cory schafer and have made this. 
+#So we have watched the first video from cory schafer and have made this. 
 #It should be good learning because it's not blindly copying cory schafer, but rather chaning it.
 
 class gpu:
-    def __init__(self, memory, cores, price, stock):
-        self.memory= memory
-        self.cores = cores
+
+    totalCards = 0
+
+    def __init__(self, name, price, company):
+        self.name = name
         self.price = price
         self.realprice = price * 3
-        self.stock = stock
+        self.company = company
 
-    def canYouGetIt(self):
-        #This works when we pass in self because self already has all the arguments from above.
-        #It has self.memory, self.cores, etc.
-        print('There are exactly ' + str(self.stock) + ' of this gpu avaliable and one costs ' + str(self.price))
-        if self.stock < 10 or self.price > 500:
-            print('You cannot get one.')
-        else:
-            print('You should probably not get one.')
+        gpu.totalCards += 1
+        #I can increase the totalCards class variable here because every time a new card is added, it will instance it.
+
+
+
+#    def canYouGetIt(self):
+#        This works when we pass in self because self already has all the arguments from above.
+#        It has self.memory, self.cores, etc.
+#        print('There are exactly ' + str(self.stock) + ' of this gpu avaliable and one costs ' + str(self.price))
+#        if self.stock < 10 or self.price > 500:
+#            print('You cannot get one.')
+#        else:
+#            print('You should probably not get one.')
 
 
 
@@ -31,6 +38,6 @@ class gpu:
 #memory argument we pass in when we create the gtx1060 is read as gtx1060.memory.
 #I feel like a genius.
 
-gtx1060 = gpu(3, 1152, 300, 0)
-print(gtx1060)
-gtx1060.canYouGetIt()
+gpu1 = gpu(gtx1060, 300, nvidia)
+print(gpu1)
+gpu_1.canYouGetIt()
